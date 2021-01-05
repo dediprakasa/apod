26165352766572f375e76a65d9907dc6bbf8f993
Dedi Prakasa <dedprakasa@gmail.com>
2021-01-05 20:42:53 +0700

    add Mapper

---
+++ Modules/Core/Sources/Core/Mapper.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dedi Prakasa on 1/5/21.
+//
+
+import Foundation
+
+public protocol Mapper {
+    associatedtype Response
+    associatedtype Entity
+    associatedtype Domain
+    
+    func transformResponseToEntity(response: Response) -> Entity
+    func transformEntityToDomain(entity: Entity) -> Domain
+}
