3a463289ba50ee5b7417d06e8ce272194b992ef3
Dedi Prakasa <dedprakasa@gmail.com>
2021-01-24 16:32:32 +0700

    add gelistpresenter

---
+++ Modules/Core/Core/Presentation/GetListPresenter.swift
@@ -0,0 +1,46 @@
+//
+//  GetListPresenter.swift
+//  Core
+//
+//  Created by Dedi Prakasa on 1/24/21.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+public class GetListPresenter<Request, Response, Interactor: UseCase>: ObservableObject
+where Interactor.Request == Request, Interactor.Response == [Response] {
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
+    private let useCase: Interactor
+    
+    @Published public var list: [Response] = []
+    @Published public var errorMessage: String = ""
+    @Published public var isLoading: Bool = false
+    @Published public var isError: Bool = false
+    
+    public init(useCase: Interactor) {
+        self.useCase = useCase
+    }
+    
+    public func getList(request: Request?) {
+        isLoading = true
+        useCase.execute(request: request)
+            .receive(on: RunLoop.main)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    self.errorMessage = error.localizedDescription
+                    self.isError = true
+                    self.isLoading = false
+                case .finished:
+                    self.isLoading = false
+                }
+            }, receiveValue: { list in
+                self.list = list
+            })
+            .store(in: &cancellables)
+    }
+}
